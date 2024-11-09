41a8acb8399d3717bd1fbd12dd185048c67006c1
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-09 14:04:55 +0530

    Add files via upload

---
+++ 0808.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Employee{
+	private:
+		string name,department,desig,performance;
+		int eid;
+		
+		void setperformance() {
+			int choice;
+			cout<<"please select performance in the following options"
+			cout<<" "
+		}
+		
+		
+		Employee(){
+			cout<<"enter eid,name,department,designation";
+			cin>>eid>>name>>department>>desig;
+			setperformance();
+		}
+		void display() {
+			cout<<"Eid:"<<eid<<endl;
+			cout<<"Name:"<<Name<<endl;
+			cout<<"Eid:"<<eid<<endl;
+			cout<<"Eid:"<<eid<<endl;
+		}
+};
+int main(){
+	Employee
+}
