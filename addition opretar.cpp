38459725482e65b1dc543ab6470a1ffa0200b764
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-11 13:35:30 +0530

    Add files via upload

---
+++ addition opretar.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+int main(){
+	int num1,num2;
+	cout<<"enter num1 and num2:";
+	cin>>num1>>num2;
+	cout<<"Addition of"<<num1<<"and"<<num2<<" :"<<num1+num2<<endl;
+	cout<<"substraction of"<<num1<<"and"<<num2<<" :"<<num1-num2<<endl;
+	cout<<"division of"<<num1<<"and"<<num2<<" :"<<(float)num1/num2<<endl;
+	cout<<"mod of"<<num1<<"and"<<num2<<" :"<<num1%num2<<endl;
+//	===================================================================
+	//typecasting:-->to convert one data-type into anothor ,this concept
+	//is known as type casting
+	//1)implicit typecasting:automotic
+	//compiler done by itself
+	//2)explictic done by program to get desict	
+}
