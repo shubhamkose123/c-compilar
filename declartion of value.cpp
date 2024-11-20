79c37d2837f4af465720aeb9eac1d4800ef51fba
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-20 12:28:54 +0530

    Add files via upload

---
+++ declartion of value.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b;  // declartion of value 
+	cout<<"Enter the value of a:";
+	cin>>a;
+	cout<<"entre the value of b:";
+	cin>>b;
+	
+	cout<<"value of a:"<<a<<" value of b:"<<b<<endl;
+	cout<<"Addition:"<<a+b;
+}
