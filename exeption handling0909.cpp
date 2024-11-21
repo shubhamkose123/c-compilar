dddf71bfee7e55e724aad507d19615e73c4f983a
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-21 11:52:06 +0530

    Add files via upload

---
+++ exeption handling0909.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+
+int main(){
+	
+//	int num,dino;
+//	try{
+//		cout<<"enter numerator:";
+//		cin>>num;
+//			cout<<"enter denominator:";
+//		cin>>dino;
+//		if(dino==0){
+//			throw dino;
+//		}
+//		cout<<"Division:"<<num/dino<<endl;
+//	}
+//	catch(int args){
+//		cout<<"you entered dinominator as:"<<args<<endl;
+//		cout<<"dinominator can't be zero";
+//	}
+    
+}
