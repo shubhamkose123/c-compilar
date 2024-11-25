a685cbfafd26fd2f1c546d073224712f6e2d3b8a
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-11-25 11:57:13 +0530

    Add files via upload

---
+++ for loop.cpp
@@ -0,0 +1,18 @@
+//condition check ->body execute ->updete
+//for(intialize_control_variable;text_expression;updation){	
+	//body of for loop}
+//ex:=>
+//        for(int i=1;i<=5;1++){
+//        	cout<<i<<endl;
+//		};
+	
+#include<iostream>
+using namespace std;
+
+int main(){
+	for(int i=1;i<=5;1++)
+	{
+       	cout<<i<<endl;
+	}
+	
+}
