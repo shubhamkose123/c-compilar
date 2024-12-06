021d09e909b312b4741bdf53558ee2847c4b4ba3
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-12-06 12:14:02 +0530

    Add files via upload

---
+++ natural numbar.cpp
@@ -0,0 +1,178 @@
+//Write a program to print all natural numbers from 1 to n
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter num";
+//	cin>>n;
+//	for(int a=1;a<=n;a++){
+//		cout<<a<<" ";
+//	}
+//}
+
+//Write a program to print all natural numbers in reverse
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter numar";
+//	cin>>n;
+//	for(int a=n;a>=1;a--){  //int 10;0>=1;10--
+//		cout<<a<<" ";
+//	}
+//}
+
+
+//Write a program to print tables
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int num;
+//	cout<<"enter teble";
+//	cin>> num;
+//	for(int i=1;i<=10;i++){
+//		cout<<i*num<<" "<<endl;
+//	}	
+//}
+
+
+//Write a program to print reverse tables
+//#include <iostream>
+//using namespace std;
+//int main(){
+//     int num;
+//     cout<<"enter tebale";
+//     cin>>num;
+//     for(int i=10;i>=1;i--){
+//     	cout<<i<<"x";
+//	 }
+
+//}
+
+
+ //Write a program to print all alphabets from a to z
+// #include <iostream>
+//using namespace std;
+//int main(){
+//
+//	for(char i='a';i<='z';i++){
+//		cout<<i<<" ";
+//	}
+//	
+//}
+
+//Write a program to print reverse alphabets from Z to A
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(char i='Z';i>='A';i--){
+//		cout<<i<<" ";
+//	}
+//}
+
+//Write a program to print all even numbers between 1 to 100	
+//	#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=1;i<=100;i++){
+//		if(i%2==0){
+//			cout<<i<<" "<<endl;
+//		}
+//		
+//	}
+//		
+//	}
+
+
+// Write a program to print all odd number between 1 to 100
+//#include<iostream>
+//using namespace std;
+//int main(){
+//
+//	for(int i=1;i<=100;i++){
+//		if(i%2!=0){
+//		cout<<i<<" "<<endl;
+//		}
+//	   }
+//	}
+
+
+//Write a program to find sum of all natural numbers between 1 to n
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int n,sum=0;
+//	cout<<"enter num";
+//	cin>>n;
+//	for(int a=1;a<=n;a++){
+//		cout<<a<<" ";
+//		sum=sum+a;
+//	}
+//	cout<<"="<<sum;
+//}
+
+
+ ///Write a program to find sum of all even numbers between 1 to n
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n,sum=0;
+//	cout<<"enter number";
+//	cin>>n;
+//	for(int i=1;i<=n;i++){
+//		if(i%2==0){
+//			cout<<i<<" ";
+//			sum=sum+i;
+//		}	
+//	}
+//	cout<<"="<<sum;
+//}
+
+//Write a program to print tables
+	
+	#include <iostream>
+using namespace std;
+int main(){
+	int num,reng;
+	cout<<"enter num and reng";
+	cin>>num>>reng;
+	for(int i=num;i<=reng;i++){ //i=2
+		for(int j=1;j<=10;j++){   // 2,3,4,5
+			cout<<j*i<<" "<<endl;
+		}	
+		cout<<endl;
+	}
+}
+
+
+//Write a program to print reverse tables
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int num,reng;
+//	cout<<"enter num and reng";
+//	cin>>num>>reng;
+//	for(int i=reng;i>=num;i--){ //i=2
+//		for(int j=1;j<=10;j++){   // 2,3,4,5
+//			cout<<j*i<<" "<<endl;
+//		}	
+//		cout<<endl;
+//	}
+//}
+
+
+
+	
+	
+
+	
+
+
+
+
+
+
+
+
