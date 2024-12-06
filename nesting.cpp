58ef927eecfd02ac8194c42ec666dbcc8e21c0b6
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-12-06 12:14:31 +0530

    Add files via upload

---
+++ nesting.cpp
@@ -0,0 +1,133 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=1;i<=3;i++){
+//		cout<<i<<" set \n ";
+//		for(int j=1;j<=10;j++){
+//			cout<<j<<" repetation\n";
+//		}
+//	}
+//}
+// i=1,1<=3,
+//j=1,2,3,4,5,6,7,8,9,10, j<=10 j=11(terminate) innrar
+//oiuter loop uptade
+//1=2 2<=3
+//j=1,2,3,4,5,6,7,8,9,10, j<=10 j=11    j=11(terminate) innrar
+//
+//
+//wap to dra pattern 
+//****           ****                   *
+//****           *   *                  **
+//****           *   *                  ***
+//****           *   *                  ****
+//****           ****                   *****
+//
+//
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=1;i<=5;i++){
+//		for(int j=1;j<=10;j++){
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//	}
+//}
+//
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=0;i<6;i++){
+//	for(int j=0;j<=i;j++){
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//	}
+//
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=0;i<6;i++){
+//	for(int j=0;j<=i;j++){
+//			cout<<(char)('A'+j)<<" ";
+//		}
+//		cout<<endl;
+//	}
+//
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=0;i<6;i++){
+//	for(int j=0;j<=i;j++){
+//		if(i ==0 || i==6-1 ||j==0 || j==i -1)
+//		cout<<"*";
+//		else
+//		cout<<"  ";
+//}
+//	cout<<endl;
+//
+//}
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int n=0;
+//	for(int i=0;i<=5;i++){
+//	for(int j=0;j<=i;j++){
+//			cout<<n<<" ";
+//			n++;
+//		}
+//		cout<<endl;
+//	}
+//
+//}
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=0;i<=5;i++){
+//	for(int j=0;j<=i;j++){
+//			cout<<i<<" ";
+//		
+//		}
+//		cout<<endl;
+//	}
+//
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	for(int i=0;i<=5;i++){
+//	for(int j=0;j<=i;j++){
+//			cout<<j<<" ";
+//		
+//		}
+//		cout<<endl;
+//	}
+//
+//}
+
+
+//wap to print tebale form renge 15 to 25
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	    for(int num=15;num<=25;num++){
+//		for(int i=1;i<=10;i++){
+//		cout<<num<<"x" <<i<<" = "<<num*i<<endl;
+//		}
+//		cout<<endl;
+//	}	
+//}
+
+
+
+
+	
+
