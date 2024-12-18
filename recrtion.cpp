e6d132efddfe25015c2d9bdcd689b4b8c0434660
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-12-18 11:40:34 +0530

    Add files via upload

---
+++ recrtion.cpp
@@ -0,0 +1,30 @@
+/*#include<iostream>
+using namespace std ;
+
+void f1(int n){
+    	if(n>5)
+    	return;
+		cout<<"this is function f1() ,current n value is:"<<n<<endl;
+	    f1(n+1); //head output
+	    cout<<"this is function f1() ,current n value is:"<<n<<endl;
+}
+int main(){
+	f1(1);
+} */
+//_______________________________________________________________________
+/*#include<iostream>
+using namespace std ;
+
+void f1(int n){
+    	if(n>5)
+    	return;
+	    f1(n+1);
+		cout<<"n:"<<n<<"----------\n";
+		f1(n+2);
+		cout<<"n:"<<n<<endl; 
+}
+int main(){
+	f1(1);
+} */
+//___________________________________________________________________
+
