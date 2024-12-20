cef0b9eee941783f1c3d06e0c9468ed37b6c5ecc
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-12-20 11:52:17 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,10 @@
+/*___________________________OOPS______________________________________________
+Class -----Collection of a data member and member function.
+          Class is a blueprint of on object.
+		  
+Object:-Instance of a class is Called on Object.		                                                            		  */
+#include<iostream>
+using namespace std;
+int main(){
+	
+}
