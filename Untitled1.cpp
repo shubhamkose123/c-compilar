578ad24d35ccdd5995512ee9db2223ee0ae6ef41
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2024-12-30 21:17:36 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,18 @@
+//friend class 
+/*We can declare friend class or function anywhere in the base class body whether its private, 
+protected or public block. It works all the same.*/
+#include<iostream>
+using namespace std;
+class sbi
+{
+	int amount=9000;
+	void balance()
+{
+	cout<<"blaance="<<amount<<"\n";z
+}
+
+
+
+
+
+ 
