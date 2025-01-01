978b9854d73b47a9a1aa2fd21bb46a4b547b6d79
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-01-01 19:37:24 +0530

    Add files via upload

---
+++ class test vikash sir.cpp
@@ -0,0 +1,20 @@
+/*#include<iostream>
+using namespace std;
+class Table{
+public:
+    static void printTable(int num) {
+        for(int i=1;i<=10;i++) {
+            cout<<num<<"*"<<i<<"="<<num*i<<endl;
+        }
+    }
+};
+int main() {
+    int num;
+    cout<<"Enter a number:";
+    cin >> num;
+    cout<<"Table of"<<num<<":"<<endl;
+    Table::printTable(num);  
+}*/
+
+
+
