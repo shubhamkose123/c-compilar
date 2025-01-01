0c05ad4b4c195b070857c8a46cad138d2d1cd770
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-01-01 19:36:52 +0530

    Add files via upload

---
+++ Composite Class vikash sir22.11.cpp
@@ -0,0 +1,49 @@
+/*1) single class-- if a progrsm having only one class its called single class
+2)anonimos class--- a class which dose nt having its own name is called anonimus calss
+ note--in anonimus calss we have to create and object while creating it.
+3)multiple class
+4)composite class*/
+
+/*#include<iostream>
+using namespace std;
+class 
+{
+	int a,b;
+	public:sum(int x,int b)
+	{
+		cout<<x*b;
+	}
+	mul(int c,int y)
+	{
+		cout<<c*y;
+	}
+} obj;
+int main(){
+	
+	obj.sum(10,23);
+	
+}*/
+
+//Composite class
+/*#include<iostream>
+using namespace std;
+	class heker{
+		public: void virus()
+		{
+			cout<<"heker class\n";
+		}
+};
+
+class user{
+	heker h;//---- composite class
+		public: void reward()
+		{
+			cout<<"you won $40000 \n";
+			h.virus();//------- composite class
+		}
+};
+int main(){
+	user cust;
+	cust.reward();*/
+
+
