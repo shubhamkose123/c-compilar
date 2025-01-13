dcf7cb2377df57ebca9413dbdd4510cc278e283f
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-01-13 13:46:05 +0530

    Add files via upload

---
+++ 07march.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string st ="apple is good ";
+    string st1="this is our python class";
+	st.replace(6,2,"are");
+	st1.replace(12,6,"c++"); 
+	cout<<st1;      
+
+    
+}
