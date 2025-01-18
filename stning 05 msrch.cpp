4f5c55255fd6a0ddc0e3e7e0087803dbde8c48ca
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-01-18 14:06:15 +0530

    Add files via upload

---
+++ stning 05 msrch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string name="aditya";
+	cout<<name<<endl;
+	cout<<"enter name:";
+	//getline(cin,name);
+	cout<<"shubham ";
+	
+	
+	return 0;
+}
