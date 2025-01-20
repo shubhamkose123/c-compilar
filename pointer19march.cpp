43f22fd1eced2c0a0b9a553cbbafb4c7d5d38f9e
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-01-20 12:49:34 +0530

    Add files via upload

---
+++ pointer19march.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+//int main(){
+//	int n=90,*p;
+//	p=&n;
+//	 cout<<"address of n:"<<p<<endl;        //cout<<"data stored n:"<<n;
+//     cout<<"data stored at n:"<<*p;       //derefrence
+//}
+                                         //pointer create, pointer derefrence--> *
+                                        //derefrence --> fatch data through pointer
+                   //{higher}                     
+//  #include<iostream>
+//  using namespace std;
+//  int main(){
+//  	int n=90,n1=999,*p,*p1;
+//	p=&n;
+//	p1=&n1;
+//	if(*p>*p1)
+//	 cout<<"higher"<<*p; 
+//	 else       
+//    cout<<"higher"<<*p1;       
+//}
+
+// #include<iostream>
+//  using namespace std;
+//  int main(){
+//  	int n=90,n1=999,*p,*p1;
+//	p=&n;
+//	p1=&n1;
+//	 
+//	 *p=*p+*p1;
+//	 cout<<*p;
+//}
+  	
+                                       
+                                        
+
+
