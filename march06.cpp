a6e4387378fde0bae46379a70b6e54fbbaff2716
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-01-20 12:49:03 +0530

    Add files via upload

---
+++ march06.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string st="apple";   
+	string st1="is good 4 health";
+	st.push_back('A');
+	st.push_back('B');
+	//st.pop_back();
+	cout<<st;
+	
+	
+
+}
