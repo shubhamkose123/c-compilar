d92cc009651f58637073dbce44a1802f7f9d80a5
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-01-27 14:09:18 +0530

    Add files via upload

---
+++ push back06.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string st="apple";   
+	string st1="is good 4 health";
+	//lenth()--->actual length
+	//capacity ()--->memory allocate
+	//length<=capacity
+	cout<<st.length()<<endl;//5
+	cout<<st.capacity()<<endl;//5
+	
+	
+
+}
