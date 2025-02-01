d65fa93b68e8204924b66999476bf532bbe315c1
Shubham Kose <157570990+shubhamkose123@users.noreply.github.com>
2025-02-01 14:25:42 +0530

    Add files via upload

---
+++ erase 07march.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string st ="apple is good ";
+    string st1="this is our python class";
+//	st.replace(6,2,"are");
+	st1.erase(6,2); // erase (startoing_pos,limit)
+	cout<<st;      
+
+    
+}
